a3697a3d6b83960dd97cf19139be8bf1f9a0ef21
migs120 <migs910@hotmail.com>
2017-07-02 14:04:00 -0700

    migstagram start off commit

---
+++ RootPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootPageViewController.swift
+//  migstagram
+//
+//  Created by Miguel Gutierrez on 7/2/17.
+//  Copyright © 2017 Miguel Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class RootPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+}
