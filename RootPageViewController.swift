13d6af1f9dbecce4edc3e174a0b31e01b9f0ce68
migs120 <migs910@hotmail.com>
2017-08-19 14:00:34 -0700

    review of changes

--- RootPageViewController.swift
+++ RootPageViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class RootPageViewController: UIViewController {
+class RootPageViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
 
+    
+    //var dataSource = UIPageViewControllerDataSource
+    
+    lazy var vcArray: [UIViewController]={
+                                        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+                                        let camVC = storyBoard.instantiateViewController(withIdentifier: "camVC")
+                                        let appVC = storyBoard.instantiateViewController(withIdentifier: "appVC")
+                                        let messVC = storyBoard.instantiateViewController(withIdentifier: "messVC")
+        
+                                        return [appVC, camVC, messVC]
+                                        }()
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+                                super.viewDidLoad()
+                             //self.dataSource  = self
+        //[[RootPageViewController] setDataSource, self];
+                               // self.delegate = self
+        
+                                if let firstViewController = self.vcArray.first{
+                                    self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+                                        }
+                                }
+    
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?{
+        
+        guard let vcIndex = vcArray.index(of: viewController) else {return nil}
+        let prevIndex = vcIndex - 1
+        guard prevIndex >= 0 else { return vcArray.last}
+        return nil}
+    
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?{return nil}
 
    
 
